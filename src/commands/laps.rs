00bf37fd0a4fa71306b0eaba36dad337472f50bf
Harm Weites <ping@harmless.industries>
2025-01-14 15:00:30 +0000

    chore: nicely show lap times

--- src/commands/laps.rs
+++ src/commands/laps.rs
@@ -2,10 +2,11 @@ use xdrk::Run;
 
 pub fn display_laps_info(run: &Run) {
     println!(
-        "{:<10} {:<20} {:<20}",
+        "{:<10} {:<20} {:<20} {}",
         "LAP".to_string(),
         "START".to_string(),
-        "DURATION".to_string()
+        "DURATION".to_string(),
+        "LAP TIME".to_string()
     );
 
     for lap in 0..run.number_of_laps() {
@@ -15,11 +16,16 @@ pub fn display_laps_info(run: &Run) {
                 let start_time = format!("{:.3}", lap_info.start());
                 let duration = format!("{:.3}", lap_info.time());
 
+                let minutes = (&lap_info.time() / 60.0).floor();
+                let secs = &lap_info.time() % 60.0;
+                let lap_time = format!("{:02}:{:06.3}", minutes as u64, secs);
+
                 println!(
-                    "{:<10} {:<20} {:<20}",
+                    "{:<10} {:<20} {:<20} {}",
                     lap_info.number(),
                     start_time,
-                    duration
+                    duration,
+                    lap_time
                 );
             }
             Err(e) => {
